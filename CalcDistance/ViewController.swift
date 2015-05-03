76c50713221737254b853cbfeca5db2d5749b51f
fuhi1983 <fuhi1983@gmail.com>
2015-05-03 17:32:16 +0900

    羽田空港から香港ビクトリアピークまでの距離を計算して表示

--- CalcDistance/ViewController.swift
+++ CalcDistance/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var distanceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 羽田空港のCLLocation
+        let hanedaLocation = CLLocation(latitude: 35.549393, longitude: 139.779839)
+        
+        // 香港ビクトリアピークのCLLocation
+        let peakLocation = CLLocation(latitude: 22.2760448, longitude: 114.1455266)
+        
+        // 羽田空港から香港ビクトリアピークまでの距離
+        let distance = hanedaLocation.distanceFromLocation(peakLocation)
+        
+        // 1000mを超える場合はキロメートで表示
+        let distanceText = distance / 1000.0 > 1.0 ?
+            "\(floor(distance / 1000.0)) キロメートル"
+            :
+            "\(floor(distance)) メートル"
+        
+        distanceLabel.text = distanceText
+        
     }
 
     override func didReceiveMemoryWarning() {
